945f367be994c51aea7a3cd7e55a52adaa42e38d
sazuxerox <sazzadhossain@live.com>
2015-12-26 01:06:21 +0600

    Initial Commit

---
+++ IndexerTest2/IndexedNames.cs
@@ -0,0 +1,40 @@
+﻿namespace IndexerTest2
+{
+    class IndexedNames
+    {
+        private string[] namelist = new string[size];
+        public static int size = 10;
+        int i;
+        public IndexedNames()
+        {
+            for ( i = 0; i < size; i++)
+            {
+                namelist[i] = "N.A";
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                string tmp;
+                if (index >= 0 && index < size - 1)
+                {
+                    tmp = namelist[index];
+                }
+                else
+                {
+                    tmp = " ";
+                }
+                return (tmp);
+            }
+            set
+            {
+                if (index >= 0 && index < size - 1)
+                {
+                    namelist[index] = value;
+                }
+            }
+        }
+    }
+}
---
+++ IndexerTest2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IndexerTest2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var names = new IndexedNames();
+
+            
+            for (int i = 0; i < 10; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Printing the names");
+
+            for (int i = 0; i < IndexedNames.size; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
